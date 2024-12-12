a557baf3bf24df8081df1ec9fd487e34597ebba1
Praveen Adithya B <157034812+pravee-n2005@users.noreply.github.com>
2024-12-12 12:00:49 +0530

    Add files via upload

---
+++ Select statements.sql
@@ -0,0 +1,19 @@
+SELECT DISTINCT FIRST_NAME, GENDER
+FROM parks_and_recreation. employee_demographics;
+
+SELECT first_name, 
+last_name, 
+birth_date, 
+age, 
+(age+10)
+FROM employee_demographics
+
+#PEMDAS	
+
+
+
+
+
+
+
+
