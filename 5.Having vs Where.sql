c1129827259a45659b72bbdf5904917b3aea5a5e
Praveen Adithya B <157034812+pravee-n2005@users.noreply.github.com>
2024-12-15 17:43:12 +0530

    Add files via upload

---
+++ 5.Having vs Where.sql
@@ -0,0 +1,18 @@
+-- Having vs Where
+
+SELECT gender, AVG (age)
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG (age) > 40;
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE occupation LIKE '%Manager%'
+GROUP BY occupation;
+
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE occupation LIKE '%Manager%'
+GROUP BY occupation
+HAVING AVG(salary) > 75000;
