09591838e486519ac6b95c8640d5bff5cb25049a
Praveen Adithya B <157034812+pravee-n2005@users.noreply.github.com>
2024-12-16 10:54:02 +0530

    Add files via upload

---
+++ 8.Unions.sql
@@ -0,0 +1,25 @@
+-- Unions - a union allows you to combine rows together from data or seperaate tables
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name, 'Old Male' AS Label	
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Old Lady' AS Label	
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name;
+
+
+
+
